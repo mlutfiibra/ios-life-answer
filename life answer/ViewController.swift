8c7bb0c90341a8f9f386a8fe2f2f469af92efb13
mlutfiibra <mlutfiibra@outlook.com>
2019-10-10 22:24:31 +0700

    Random

--- life answer/ViewController.swift
+++ life answer/ViewController.swift
@@ -9,12 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let ballCollection = ["ball1", "ball2", "ball3", "ball4", "ball5"]
+    
+    var randomBallNumber : Int = 0
 
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        randomBallNumber = Int(arc4random_uniform(5))
         // Do any additional setup after loading the view.
+        imageView.image = UIImage(named : ballCollection[randomBallNumber])
     }
-
-
+    
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+    }
+    
 }
 
