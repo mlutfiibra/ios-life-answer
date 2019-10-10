c67570b9cbf566d28a825f1db9eb67a13009dd62
mlutfiibra <mlutfiibra@outlook.com>
2019-10-10 22:28:56 +0700

    Implement gesture motion shake

--- life answer/ViewController.swift
+++ life answer/ViewController.swift
@@ -13,18 +13,26 @@ class ViewController: UIViewController {
     let ballCollection = ["ball1", "ball2", "ball3", "ball4", "ball5"]
     
     var randomBallNumber : Int = 0
+    
+    func updateBallImage() {
+        randomBallNumber = Int(arc4random_uniform(5))
+        
+        imageView.image = UIImage(named : ballCollection[randomBallNumber])
+    }
 
     @IBOutlet weak var imageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        randomBallNumber = Int(arc4random_uniform(5))
-        // Do any additional setup after loading the view.
-        imageView.image = UIImage(named : ballCollection[randomBallNumber])
+        updateBallImage()
     }
     
     @IBAction func askButtonPressed(_ sender: UIButton) {
+        updateBallImage()
     }
     
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        updateBallImage()
+    }
 }
 
