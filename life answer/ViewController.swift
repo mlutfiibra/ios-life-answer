bc1040d3e2057952c308f9bdae9446285d75e029
mlutfiibra <mlutfiibra@outlook.com>
2019-10-10 19:51:28 +0700

    Initial Commit

---
+++ life answer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  life answer
+//
+//  Created by lutfi on 10/10/19.
+//  Copyright © 2019 mlutfiibra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
